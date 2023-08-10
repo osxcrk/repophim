83c2271fbfd56aa7931a00df5f3268e470416791
netcrk <33061227+netcrk@users.noreply.github.com>
2023-08-10 13:55:40 +0700

    Update Vlxx.kt

--- Vlxx/src/main/kotlin/com/jacekun/Vlxx.kt
+++ Vlxx/src/main/kotlin/com/jacekun/Vlxx.kt
@@ -17,7 +17,7 @@ class Vlxx : MainAPI() {
     private val globaltvType = TvType.NSFW
 
     override var name = "Vlxx"
-    override var mainUrl = "https://vlxx.net"
+    override var mainUrl = "https://vnxx.app"
     override val supportedTypes = setOf(TvType.NSFW)
     override val hasDownloadSupport = false
     override val hasMainPage = true
